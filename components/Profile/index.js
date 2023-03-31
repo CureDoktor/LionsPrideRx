fd4cbc09b0a688f6ae6ed71d22181538f5307628
Nikola Ćurčić <62150474+CureDoktor@users.noreply.github.com>
2023-03-31 22:36:20 +0200

    adding functionality

--- components/Profile/index.js
+++ components/Profile/index.js
@@ -1,6 +1,72 @@
 import Image from "next/image";
 import Link from "next/link";
+import React, { useState, useContext, useEffect } from "react";
+import AuthContext from "../../store/auth-context";
+import Axios from "axios";
 export default function Profile() {
+  const authCtx = useContext(AuthContext);
+  const gettingUserInfo = async () => {
+    const route = "/api/user/getUserInfo";
+
+    try {
+      const rese = await Axios.post(route, { Token: authCtx.Token() })
+        .then((res) => {
+          setUserInfo(res.data);
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("Not Good!");
+        });
+    } catch (err) {
+      alert("Something went wrong!" + err);
+    }
+  };
+
+  useEffect(() => {
+    gettingUserInfo();
+  }, []);
+
+  const [save, setSave] = useState(false);
+  const [billInfo, setBillInfo] = useState(false);
+  const [payInfo, setPayInfo] = useState(false);
+  const saveEnable = () => {
+    setSave(true);
+  };
+
+  const [userInfo, setUserInfo] = useState({
+    auth_customer_payment_id: null,
+    auth_customer_profile_id: null,
+    billingAddress: null,
+    billingCityId: null,
+    billingCityName: null,
+    billingFirstName: null,
+    billingLastName: null,
+    billingSameAsShipping: null,
+    billingState: null,
+    billingZip: null,
+    creditCardNumber: null,
+    cvv: null,
+    date_of_birth: null,
+    email_notification: null,
+    expirationDate: null,
+    firstName: " ",
+    id: 1,
+    lastName: " ",
+    patient_id: null,
+    payment_processor: null,
+    phone: "",
+    shippingAddress: "",
+    shippingCityId: "",
+    shippingCityName: "",
+    shippingMethod: null,
+    shippingState: "",
+    shippingZip: "",
+    sms_notification: null,
+    user_id: 2,
+    verification_job_id: null,
+    verification_status: null,
+    verification_token: null,
+  });
   return (
     <>
       <div>
@@ -11,7 +77,9 @@ export default function Profile() {
                 <div className="col col-12 col-lg-5 col-xl-4">
                   <div className="profile__cell pe-lg-4">
                     <h4>Personal Information</h4>
-                    <p className="h4">John Doe</p>
+                    <p className="h4">
+                      {userInfo.firstName} {userInfo.lastName}
+                    </p>
                     <table className="table">
                       <tbody>
                         <tr>
@@ -20,11 +88,11 @@ export default function Profile() {
                         </tr>
                         <tr>
                           <th>Phone Number:</th>
-                          <td>(516)286-0320</td>
+                          <td>{userInfo.phone}</td>
                         </tr>
                         <tr>
                           <th>Date Of Birth:</th>
-                          <td>June 01, 1982</td>
+                          <td>{userInfo.date_of_birth}</td>
                         </tr>
                         <tr>
                           <th>Password:</th>
