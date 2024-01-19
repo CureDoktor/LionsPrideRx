849e1153fd1c9ed298caf38459f78013db169e2f
dusantomic98 <dusan.tomic998@gmail.com>
2024-01-19 21:04:15 +0100

    Upgrade sildenafil

---
+++ src/pages/slidenafil-upgrade/components/Card.js
@@ -0,0 +1,115 @@
+import React, { useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import Image from "next/image";
+import checkmark from "@public/img/image 139.png";
+import checkmarkSelected from "@public/img/home/checkmark.png";
+import styles from "./styles.module.scss";
+const Card = () => {
+  const [selected, setSelected] = useState(null);
+  const cards = [
+    {
+      id: 1,
+      title: "3 Month Package",
+      doses: "12 doses",
+      name: "Sildenafil",
+      savings: 10,
+      discount: 9.6,
+      price: 86.4,
+    },
+    {
+      id: 2,
+      title: "6 Month Package",
+      doses: "24 doses",
+      name: "Sildenafil",
+      savings: 20,
+      discount: 48,
+      price: 192,
+    },
+    {
+      id: 3,
+      title: "12 Month Package",
+      doses: "48 doses",
+      name: "Sildenafil",
+      savings: 30,
+      discount: 158.4,
+      price: 369.6,
+    },
+  ];
+
+  return (
+    <div>
+      <h2 className={styles.upgradeHeading}>Select your Upgrade Below</h2>
+      <Container className={styles.cont}>
+        <Row>
+          <Col className={styles.Card}>
+            {cards.map((c) => (
+              <div
+                key={c.id}
+                className={`${styles.singleCard} ${
+                  selected === c.id ? styles.selected : ""
+                }`}
+              >
+                <h3 className={`${styles.cardHeading} `}>{c.title}</h3>
+                <ul className={styles.benefits}>
+                  <li className={`${styles.doses}`}>
+                    <Image
+                      className={`${styles.checkmark}`}
+                      src={selected === c.id ? checkmarkSelected : checkmark}
+                      alt="Checked"
+                    ></Image>
+                    <p className={`${styles.dosesText}`}>
+                      <strong>{c.doses}</strong> of 25.0mg {c.name} (oral -
+                      tablet)
+                    </p>
+                  </li>
+                  <li className={`${styles.savings}`}>
+                    <Image
+                      className={`${styles.checkmark}`}
+                      src={selected === c.id ? checkmarkSelected : checkmark}
+                      alt="Checked"
+                    ></Image>
+                    <div>
+                      <p className={`${styles.savePercent}`}>
+                        {c.savings}% Savings
+                      </p>
+                      <p>
+                        <strong>({c.discount}$ discount)</strong>
+                      </p>
+                    </div>
+                  </li>
+                  <li className={`${styles.price} `}>
+                    <Image
+                      className={`${styles.checkmark} `}
+                      src={selected === c.id ? checkmarkSelected : checkmark}
+                      alt="Checked"
+                    ></Image>
+                    <div>
+                      <p className={`${styles.upgradePrice} `}>
+                        Only pay additional
+                      </p>
+                      <strong>${c.price}</strong>
+                    </div>
+                  </li>
+                </ul>
+                <div className={`${styles.buttonWrap} `}>
+                  <button
+                    onClick={() => setSelected(c.id)}
+                    className={`${styles.selectBtn} `}
+                  >
+                    {selected === c.id ? "Selected" : "Select"}
+                  </button>
+                </div>
+              </div>
+            ))}
+          </Col>
+        </Row>
+        <div className={styles.pckgWrap}>
+          <button className={styles.upgradePckg}>
+            Choose your <span className={styles.upper}>upgrade</span> package
+          </button>
+        </div>
+      </Container>
+    </div>
+  );
+};
+export default Card;
---
+++ src/pages/slidenafil-upgrade/slidenafilUpgrade.js
@@ -0,0 +1,131 @@
+import React from "react";
+import Image from "next/image";
+import chartsrc from "@public/img/home/CHART.png";
+import { Container, Row, Col } from "react-bootstrap";
+import styles from "../slidenafil-upgrade/styles.module.scss";
+import Card from "./components/Card";
+import FDA from "@public/img/home/image 145.png";
+import savingsImage from "@public/img/home/exponential savings.png";
+const slidenafilUpgrade = () => {
+  return (
+    <>
+      <Container className="subHeader">
+        <Row>
+          <Col>
+            <p className={styles.oneStep}>
+              You are one step away from completing your order!
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p className={styles.alert}>
+              Please do not close or navigate away from this page.
+            </p>
+          </Col>
+        </Row>
+      </Container>
+      <hr className={styles.line}></hr>
+      <Container>
+        <Row>
+          <Col>
+            <h1 className={styles.mainHeading}>Exclusive One- Time Offer</h1>
+          </Col>
+          <Row>
+            <p className={styles.saveBig}>
+              Upgrade your treatment package and SAVE BIG!
+            </p>
+          </Row>
+          <Row className={styles.ChartImage}>
+            <Image src={chartsrc} className={styles.Chart}></Image>
+          </Row>
+          <Row>
+            <h2 className={styles.h2head}>
+              9 out of 10 customers upgrade their order!
+            </h2>
+          </Row>
+          <Row className={styles.parWrap}>
+            <p className={styles.upgradePar}>
+              By upgrading your order, you’ll get the complete one-month supply
+              of your chosen treatment plan plus your medication refills for the
+              coming months. Lion’s Pride Rx’s upgraded packages offer the
+              convenience of having your refills shipped in advance, allowing
+              you to maintain top-level confidence and readiness all the time.
+            </p>
+          </Row>
+        </Row>
+      </Container>
+      <diV className={styles.bg}>
+        <Card></Card>
+      </diV>
+
+      <div className={styles.linkWrap}>
+        <a className={styles.noUpgrade} href="">
+          No, do not upgrade my order. Proceed with my monthly treatment plan
+        </a>
+      </div>
+      <div className={styles.Wrap}>
+        <div className={styles.pWrap}>
+          <p className={styles.upgradeTxt}>
+            Your upgrade today grants you incredible <strong>SAVINGS</strong> ,
+            plus the <strong>CONVENIENCE</strong> of having{" "}
+            <strong>
+              FDA-approved Generic Viagra (Sildenafil Citrate oral – tablet)
+            </strong>{" "}
+            on hand and <strong>READY</strong> for your E.D. needs in the coming
+            months!
+          </p>
+        </div>
+        <h2 className={styles.sildenafilCitrate}>
+          Upgrade now to get Sildenafil Citrate tablets at our LOWEST rates!
+        </h2>
+        <Container>
+          <Row>
+            <Col>
+              <ul className={styles.listWrap}>
+                <li className={styles.liItem}>
+                  <Image className={styles.markApproved} src={FDA}></Image>
+                  <p className={styles.Item1}>FDA-approved E.D. medication</p>
+                </li>
+                <li className={styles.liItem}>
+                  <Image className={styles.markApproved} src={FDA}></Image>
+                  <p className={styles.Item2}>Prime Sexual Health</p>
+                </li>
+                <li className={styles.liItem}>
+                  <Image className={styles.markApproved} src={FDA}></Image>
+                  <p className={styles.Item3}>
+                    Premium-quality E.D. medication
+                  </p>
+                </li>
+              </ul>
+              <div className={styles.marks}></div>
+            </Col>
+            <Row>
+              <p className={styles.dontMiss}>
+                Don’t miss this incredible ONE TIME offer to UPGRADE your order!
+              </p>
+            </Row>
+          </Row>
+        </Container>
+        <div className={styles.savingsCont}>
+          <Image className={styles.savingsImage} src={savingsImage}></Image>
+          <div>
+            <h3 className={styles.bestValueHeading}>
+              Get the BEST VALUE for your money and upgrade now to a BIGGER
+              package!
+            </h3>
+            <p className={styles.bestValueTxt}>
+              LionsPrideRx’s upgraded packages ensure that you are READY for
+              action, anytime – anywhere!
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.secondCard}>
+        <Card></Card>
+      </div>
+    </>
+  );
+};
+
+export default slidenafilUpgrade;
